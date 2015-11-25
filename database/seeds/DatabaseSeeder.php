c27f56ccc00fff1db2dd774f0dced9cb8f06b845
Loïc Parent <loicparent@MacBook-Pro-de-Loic.local>
2015-11-25 21:53:57 +0100

    Use seeder
    utiliser les « seeder » pour peupler automatiquement la base de donnée
    une fois que je fais un refresh

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        DB::table('users')->insert([
+            'name' => 'prof',
+            'email' => 'prof@prof.com',
+            'password' => 'password',
+            'status' => 1,
+        ]);
+
         Model::unguard();
 
         // $this->call(UserTableSeeder::class);
