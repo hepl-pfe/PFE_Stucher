0204cb0282f167a2fef5e2387dc01fd7275ae54a
Loïc Parent <loicparent@MacBook-Pro-de-Loic.local>
2015-11-30 20:11:39 +0100

    Add a back button to the seance page

--- resources/views/seance/viewSeance.blade.php
+++ resources/views/seance/viewSeance.blade.php
@@ -5,4 +5,5 @@
     @section( 'content' )
     @section( 'title', $title )
 	<h1>{{$title}}</h1>
+	<a class="btn btn-warning" href="{!! action( 'CourseController@view', [ "id" => $seance->course_id, "action" => 1 ] ) !!}"><—</a>
 @stop
