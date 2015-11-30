9c6aa86d642b5d949f4b85b3443398f1340cb94b
Loïc Parent <loicparent@MacBook-Pro-de-Loic.local>
2015-11-30 15:02:01 +0100

    Add some changes
    => back button
    => div -> ul

--- public/js/main.js
+++ public/js/main.js
@@ -3,23 +3,21 @@ jQuery( function($) {
 	$( '#datepicker' ).datepicker(
 		{
 			dateFormat: 'yy-mm-dd',
-			minDate: 0
+			minDate: 0,
 		}
 	);
 
-	$.timepicker.regional['fr']
+
 	$('#start_hours').timepicker(
 	 {
-	 	timeFormat: 'hh:mm',
-	 	hourMin: 8,
-	 	hourMax: 16,
+	 	hourMin: 7,
+	 	hourMax: 17,
 	 }
 	);
 	$('#end_hours').timepicker(
 	 {
-	 	timeFormat: 'hh:mm',
-	 	hourMin: 8,
-	 	hourMax: 16,
+	 	hourMin: 7,
+	 	hourMax: 17,
 	 }
 	);
 } );
--- resources/views/courses/indexAllCourses.blade.php
+++ resources/views/courses/indexAllCourses.blade.php
@@ -2,9 +2,10 @@
 	@section('title', $title)
     @section( 'content' )
     <h2>Tous les cours existants</h2>
-	<div class="list-group">
+    <a class="btn btn-warning" href="{!! action( 'CourseController@index' ) !!}"><—</a><br><br>
+	<ul class="list-group">
 		@foreach ($courses as $course)
 			<li class="list-group-item well well-lg"><a href="{!! action( 'CourseController@view', [ 'id' => $course->id, 'action' => 2 ] ) !!}">{{ $course->title }} ({{ $course->group }})</a></li>
 		@endforeach
-	</div>
+	</ul>
 @stop
