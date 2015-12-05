04091376ea9e106c4ba900eb7bd10a6abff503f2
Loïc Parent <loicparent@MacBook-Pro-de-Loic.local>
2015-12-05 12:41:36 +0100

    Add some change in indexTeacherCourses page
    - Show the title of the group to better identifie the group
    - Add a button to create a Test of Work

--- resources/views/courses/indexTeacherCourses.blade.php
+++ resources/views/courses/indexTeacherCourses.blade.php
@@ -6,8 +6,8 @@
 			<span class="caret"></span></button>
 			<ul class="dropdown-menu dropdown-menu-right">
 				<li><a href="{!! action( 'CourseController@create' ) !!}">Un cours</a></li>
-				<li><a href="{!! action( 'CourseController@addWork' ) !!}">Un devoir</a></li>
-				<li><a href="{!! action( 'CourseController@addTest' ) !!}">Une interrogation</a></li>
+				<li><a href="{!! action( 'WorkController@create' ) !!}">Un devoir</a></li>
+				<li><a href="{!! action( 'TestController@create' ) !!}">Une interrogation</a></li>
 				<li><a href="{!! action( 'CourseController@addNews' ) !!}">Une notification</a></li>
 			</ul>
 		</div>
@@ -16,7 +16,7 @@
 		<h2>Tous mes cours</h2>
 		<ul class="list-group">
 			@foreach ($courses as $course)
-				<li class="list-group-item well well-lg"><a href="{!! action( 'CourseController@view', [ 'id' => $course->id, 'action' => 1 ] ) !!}">{{ $course->title }}</a></li>
+				<li class="list-group-item well well-lg"><a href="{!! action( 'CourseController@view', [ 'id' => $course->id, 'action' => 1 ] ) !!}">{{ $course->title }} groupe {{ $course->group }}</a></li>
 			@endforeach
 	    	<li class="list-group-item well-lg"><a href="{!! action( 'CourseController@create' ) !!}">Créer un nouveau cours</a></li>
     	</ul>
