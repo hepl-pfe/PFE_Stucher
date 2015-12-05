df423cc4405fd49ee2f8c20fb0fa04d403b199e6
Loïc Parent <loicparent@MacBook-Pro-de-Loic.local>
2015-12-05 12:34:33 +0100

    work and test pages
    - view create test
    - view create work
    
    - view add test
    - view add work

---
+++ resources/views/test/createTest.blade.php
@@ -0,0 +1,56 @@
+@extends( 'layout' )
+	@section('title', $title)
+    @section( 'content' )
+    <h2><?php echo $title; ?></h2>
+    <br>
+    <a class="btn btn-success" href="">Récupérer un modèle existant</a>
+    <br>
+    <br>
+	<form action="" method="post">
+        <div class="form-group">
+            <label for="course">Pour quel cours?</label>
+            <select class="form-control" name="course" id="course">
+            @foreach ($allCourses as $singleCourse)
+                <option value="{{ $singleCourse->id }}" 
+                @if( isset($course) )
+                    @if( $singleCourse->id == $course[0]->id )
+                    selected="selected" 
+                    @endif 
+                @endif 
+                >{{ $singleCourse->title }} groupe {{ $singleCourse->group }}</option>
+            @endforeach
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="seance">Pour quel séance de cours?</label>
+            <select class="form-control" name="seance" id="seance">
+            @foreach ($allSeances as $singleSeance)
+                <option value="{{ $singleSeance->id }}" 
+                @if( $singleSeance->id == $seance->id )
+                selected="selected" 
+                @endif >Séance du {{ $seance->start_hours->formatLocalized('%A %d %B %Y') }} de {{ $seance->start_hours->formatLocalized('%Hh%M') }} à {{ $seance->end_hours->formatLocalized('%Hh%M') }}</option>
+            @endforeach
+            </select>
+        </div>
+
+        <div class="form-group">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <label for="title">Ajouter le titre à l'interrogation</label>
+            <input type="text" class="form-control" name="title" id="title" placeholder="ex: test de propabilité">
+        </div>
+
+		<div class="form-group">
+			<label for="descr">Description de l'interrogation</label>
+			<textarea class="form-control" name="descr" id="descr" cols="30" rows="10" placeholder="ex: La matière de cette interrogation portera sur…"></textarea>
+		</div>
+		<div class="form-group">
+			<label for="file">Fichier joins (facultatif - PDF, image ou Word)</label>
+			<input type="file" id="file" name="file">
+		</div>
+		<div class="form-group text-center">
+            <a class="btn btn-warning" href="{!! action( 'SeanceController@view', ['id' => $seance->id] ) !!}">Annuler</a>
+			<input type="submit" class="btn btn-primary" value="Valider l'interrogation">
+		</div>
+	</form>
+@stop
