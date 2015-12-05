e8508ace94e300bec90408e9a12468334d6d6045
Loïc Parent <loicparent@MacBook-Pro-de-Loic.local>
2015-12-05 12:30:36 +0100

    Create Test (TestController)

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,12 +6,44 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Course;
+use App\Seance;
+use \Input;
+use App\Test;
 
 class TestController extends Controller
 {
-    public function create( $id ) {
-        $title = 'Créer une interrogation';
-        $courses = Course::where( 'teacher_id', '=', \Auth::user()->id )->get();
-        return view('test/createTest', ['title' => $title, 'id' => $id, 'courses'=> $courses]);
+    public function create( $id = null, $info = null ) {
+    	$title = 'Créer une interrogation';
+    	$allCourses = Course::where( 'teacher_id', '=', \Auth::user()->id )->get();
+    	if($id == null) {
+    		return view('test/createTest', ['title' => $title, 'allCourses' => $allCourses]);
+    		die('ok sans information concernant le cours ou la séance');
+    	}
+    	if($id != null) {
+    		 if( $info == 'course' ) {
+    		 	$course = Course::findOrFail( $id );
+    		 	return view('test/createTest', ['title' => $title, 'allCourses' => $allCourses, 'course' => $course]);
+    			die('ok sans information concernant la séance');
+    		 }
+    		 if( $info == 'seance' ) {
+    		 	setlocale( LC_ALL, 'fr_FR');
+    		 	$seance = Seance::findOrFail( $id );
+		    	$allSeances = Seance::where( 'course_id', '=', $seance->course_id )->get();
+		        $course = Course::where( 'id', '=', $seance->course_id )->get();
+		        return view('test/createTest', ['title' => $title, 'seance' => $seance, 'course'=> $course, 'allCourses' => $allCourses, 'allSeances' => $allSeances]);
+    		 }
+    	}
+    }
+
+    public function store() {
+        $works = Test::create([
+            'seance_id' => Input::get('seance'),
+            'title' => Input::get('title'),
+            //'file' => Input::get('file'),
+            'description' => Input::get('descr')
+            ]);
+        
+        return redirect()->route('viewSeance', ['id' => Input::get('seance')]);
     }
 }
