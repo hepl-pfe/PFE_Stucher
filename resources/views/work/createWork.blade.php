4aa52d611252b757033e171247f6b20008f67979
Loïc Parent <loicparent@MacBook-Pro-de-Loic.local>
2015-12-06 21:53:35 +0100

    Add some changes
    -> check user
    -> check if a course is selected
    -> check if a seance is selected
    -> check which king of back button displayed
    -> add some change such as change test to work (after copy/paste)

--- resources/views/work/createWork.blade.php
+++ resources/views/work/createWork.blade.php
@@ -10,11 +10,21 @@
         <div class="form-group">
             <label for="course">Pour quel cours?</label>
             <select class="form-control" name="course" id="course">
+            
             @foreach ($allCourses as $singleCourse)
                 <option value="{{ $singleCourse->id }}" 
-                @if( $singleCourse->id == $course[0]->id )
-                selected="selected" 
-                @endif >{{ $singleCourse->title }} groupe {{ $singleCourse->group }}</option>
+                @if( isset( $course ) )
+                    @if( isset( $seance ) )
+                        <?php $courseId = $course[0]->id; ?>
+                    @else
+                        <?php $courseId = $course->id; ?>
+                    @endif
+                    <?php $singleCourseId = $singleCourse->id; ?>
+                        @if( $singleCourseId == $courseId )
+                            selected="selected"
+                        @endif                    
+                @endif 
+                >{{ $singleCourse->title }} groupe {{ $singleCourse->group }}</option>
             @endforeach
             </select>
         </div>
@@ -22,31 +32,40 @@
         <div class="form-group">
             <label for="seance">Pour quel séance de cours?</label>
             <select class="form-control" name="seance" id="seance">
-            @foreach ($allSeances as $singleSeance)
-                <option value="{{ $singleSeance->id }}" 
-                @if( $singleSeance->id == $seance->id )
-                selected="selected" 
-                @endif >Séance du {{ $seance->start_hours->formatLocalized('%A %d %B %Y') }} de {{ $seance->start_hours->formatLocalized('%Hh%M') }} à {{ $seance->end_hours->formatLocalized('%Hh%M') }}</option>
-            @endforeach
+                @foreach ($allSeances as $singleSeance)
+                    <option value="{{ $singleSeance->id }}" 
+                    @if ( isset( $seance ) )
+                        <?php $singleSeanceId = $singleSeance->id;
+                          $seanceId = $seance->id; ?>
+                        @if( $singleSeanceId == $seanceId )
+                    selected="selected" 
+                    @endif @endif >Séance du {{ $singleSeance->start_hours->formatLocalized('%A %d %B %Y') }} de {{ $singleSeance->start_hours->formatLocalized('%Hh%M') }} à {{ $singleSeance->end_hours->formatLocalized('%Hh%M') }}</option>
+                @endforeach
             </select>
         </div>
 
         <div class="form-group">
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <label for="title">Ajouter le titre du devoir</label>
+            <label for="title">Ajouter le titre au devoir</label>
             <input type="text" class="form-control" name="title" id="title" placeholder="ex: test de propabilité">
         </div>
 
         <div class="form-group">
             <label for="descr">Description du devoir</label>
-            <textarea class="form-control" name="descr" id="descr" cols="30" rows="10" placeholder="ex: La matière de cette interrogation portera sur…"></textarea>
+            <textarea class="form-control" name="descr" id="descr" cols="30" rows="10" placeholder="ex: La matière de ce devoir portera sur…"></textarea>
         </div>
         <div class="form-group">
             <label for="file">Fichier joins (facultatif - PDF, image ou Word)</label>
             <input type="file" id="file" name="file">
         </div>
         <div class="form-group text-center">
-            <a class="btn btn-warning" href="{!! action( 'SeanceController@view', ['id' => $seance->id] ) !!}">Annuler</a>
+            @if (isset( $seance ))
+                <a class="btn btn-warning" href="{!! action( 'SeanceController@view', ['id' => $seance->id] ) !!}">Annuler</a>
+            @elseif ( isset( $course ) )
+                <a class="btn btn-warning" href="{!! action( 'CourseController@view', ['id' => $course->id, 'status' => 1] ) !!}">Annuler</a>
+            @else
+                <a class="btn btn-warning" href="{!! URL::previous() !!}">Annuler</a>
+            @endif
             <input type="submit" class="btn btn-primary" value="Valider le devoir">
         </div>
     </form>
