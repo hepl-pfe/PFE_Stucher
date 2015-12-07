5a3f86aa0d4223ca39e4a2109f795f20ddddb078
Loïc Parent <loicparent@MacBook-Pro-de-Loic.local>
2015-12-07 16:15:31 +0100

    Remove the static elements from notification page

--- resources/views/pages/notification.blade.php
+++ resources/views/pages/notification.blade.php
@@ -5,17 +5,11 @@
 	@if( Auth::user()->status == 1 )
 		<a class="btn btn-primary" style="margin: 1em;" href="{!! action( 'CourseController@addNews' ) !!}">Ajouter une notification</a>	
 		<ul class="list-group">
-			<li class="list-group-item">Nouveau message de Grégory Lemmens (du cours de Français)<a href="{{ action( 'PageController@message' ) }}" class="btn badge">Voir le message</a></li>
-			<li class="list-group-item">
-			<a href="">Jonathan Petit</a> demande l’accès au cours de <a href="{{ action( 'CourseController@view', [ 'id' => 18 ] ) }}">Néérlandais 3TQ</a><a class="btn badge btn-danger">Refuser</a><a class="btn badge btn-success">Ajouter</a>
-			</li>
+			<li class="list-group-item">Aucune notification pour le moment</li>
 		</ul>
 	@elseif( Auth::user()->status == 2 )
 		<ul class="list-group">
-			<li class="list-group-item">M. Gerrard sera absent le 20/10/16</li>
-			<li class="list-group-item">Nouveau message de M. David<a href="{{ action( 'PageController@message' ) }}" class="btn badge">Voir le message</a></li>
-			<li class="list-group-item">Nouveau devoir pour le 20/10/16 <a href="{{ action( 'PageController@planning' ) }}" class="btn badge">Voir le devoir</a></li>
-			<li class="list-group-item">Votre demande d’ajout au cours de Néerlandais de M. Poinvert à été accepter<a href="{{ action( 'CourseController@view', ['id' => 18, 'action' => 1] ) }}" class="btn badge">Accéder au cours</a></li>
+			<li class="list-group-item">Aucune notification pour le moment</li>
 		</ul>
 	@endif
 @endsection
