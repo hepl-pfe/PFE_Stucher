d7212fa85885eadf82c0a99a1767ac03b7e9695b
Loïc Parent <loic1994@hotmail.com>
2015-12-08 16:23:54 +0100

    Set the form to find a course by token

--- resources/views/courses/indexAllCourses.blade.php
+++ resources/views/courses/indexAllCourses.blade.php
@@ -3,9 +3,11 @@
     @section( 'content' )
     <h2>Tous les cours existants</h2>
     <a class="btn btn-warning" href="{!! action( 'CourseController@index' ) !!}"><—</a><br><br>
-    <form action="" method="post" class="form-group">
+    <form action="{!! action( 'CourseController@getByToken') !!}" method="post" class="form-group">
     	<label for="searchToken">Code d'accès</label>
     	<input id="searchToken" name="searchToken" type="text" placeholder="ex: 2345D3">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input type="submit" value="Rechercher le cours">
     </form>
 	<ul class="list-group">
 		@foreach ($courses as $course)
