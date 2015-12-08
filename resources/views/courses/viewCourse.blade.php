79809db778637d02e610ad1c468ab6a330082c14
Loïc Parent <loic1994@hotmail.com>
2015-12-08 14:52:40 +0100

    Add some changes
    -> edit seances
    -> delete seances

--- resources/views/courses/viewCourse.blade.php
+++ resources/views/courses/viewCourse.blade.php
@@ -82,7 +82,29 @@
 			      			<a class="btn btn-primary" href="{!! action( 'SeanceController@create', ['id' => $id] ) !!}">Créer des séances de cours</a>
 			      		@else
 				      		@foreach( $seances as $seance )
-				      			<a href="{!! action( 'SeanceController@view', ['id' => $seance->id] ) !!}" class="btn btn-success">{{ $seance->start_hours->formatLocalized('%A %d %B %Y') }} de {{ $seance->start_hours->formatLocalized('%Hh%M') }} à {{ $seance->end_hours->formatLocalized('%Hh%M') }}</a><br><br>
+				      			<a href="{!! action( 'SeanceController@view', ['id' => $seance->id] ) !!}" class="btn btn-success">{{ $seance->start_hours->formatLocalized('%A %d %B %Y') }} de {{ $seance->start_hours->formatLocalized('%Hh%M') }} à {{ $seance->end_hours->formatLocalized('%Hh%M') }}</a>
+				      			<a class="btn btn-warning" href="{!! action( 'SeanceController@edit', [ "id" => $seance->id ] ) !!}">Modifier</a>
+								<!-- Modal -->
+								<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#{{ $seance->id }}">X</button>
+								<div id="{{ $seance->id }}" class="modal fade" role="dialog">
+									<div class="modal-dialog">
+										<div class="modal-content">
+											<div class="modal-header">
+												<button type="button" class="close" data-dismiss="modal">&times;</button>
+												<h4 class="modal-title">Voulez-vous vraiment supprimer cette séance?</h4>
+											</div>
+											<div class="modal-body">
+												<p>Attention, c'est irréversible!</p>
+												<p>En supprimant cette séance, vous supprimerez tous les devoirs et interrogations s'y rapportant.</p>
+											</div>
+											<div class="modal-footer">
+												<a href="{!! action( 'SeanceController@delete', [ "id" => $seance->id, "course_id" => $seance->course_id ] ) !!}" class="btn btn-danger">Oui</a>
+												<button type="button" class="btn btn-default" data-dismiss="modal">Non</button>
+											</div>
+										</div>
+									</div>
+								</div>
+				      			<br><br>
 				      		@endforeach
 				      		
 				      		@if ( \Auth::user()->status == 1 )
