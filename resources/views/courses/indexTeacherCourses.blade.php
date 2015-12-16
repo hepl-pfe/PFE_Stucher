f6930e06d51381b06938ee7b51f967d9df68c76e
Loïc Parent <loic1994@hotmail.com>
2015-12-16 09:57:10 +0100

    Change the design of the indexAllCourse page

--- resources/views/courses/indexTeacherCourses.blade.php
+++ resources/views/courses/indexTeacherCourses.blade.php
@@ -1,26 +1,21 @@
 @extends( 'layout' )
 	@section('title', $title)
     @section( 'content' )
-		<div class="dropdown text-right">
-			<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Ajouter
-			<span class="caret"></span></button>
-			<ul class="dropdown-menu dropdown-menu-right">
-				<li><a href="{!! action( 'CourseController@create' ) !!}">Un cours</a></li>
-				<li><a href="{!! action( 'WorkController@create' ) !!}">Un devoir</a></li>
-				<li><a href="{!! action( 'TestController@create' ) !!}">Une interrogation</a></li>
-				<li><a href="{!! action( 'CourseController@addNews' ) !!}">Une notification</a></li>
-			</ul>
-		</div>
+		<h2 class="pageTitle">Tous mes cours</h2>
 
-
-		<h2>Tous mes cours</h2>
-		<ul class="list-group">
+		<ul>
 			@if ( $courses->count() == null )
-				<li class="list-group-item well well-lg">Aucun cours pour le moment</li>
+				<li class="">Aucun cours pour le moment</li>
 			@endif
 			@foreach ($courses as $course)
-				<li class="list-group-item well well-lg"><a href="{!! action( 'CourseController@view', [ 'id' => $course->id, 'action' => 1 ] ) !!}">{{ $course->title }} groupe {{ $course->group }}</a></li>
+				<li class="list__round">
+					<a href="{!! action( 'CourseController@view', [ 'id' => $course->id, 'action' => 1 ] ) !!}">
+						<span class="list__name">{{ $course->title }}</span> <span class="list__group">groupe {{ $course->group }}</span>
+					</a>
+				</li>
 			@endforeach
-	    	<li class="list-group-item well-lg"><a href="{!! action( 'CourseController@create' ) !!}">Créer un nouveau cours</a></li>
+	    		<li class="list__round list__round--empty">
+	    			<a href="{!! action( 'CourseController@create' ) !!}">Créer un nouveau cours</a>
+	    		</li>
     	</ul>
 @stop
