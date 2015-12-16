0f2bb879ce128c6db0c6e06c1600baaa6c0a4548
Loïc Parent <loic1994@hotmail.com>
2015-12-16 10:01:09 +0100

    Create a new Controller => CalendarController

---
+++ app/Http/Controllers/CalendarController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Course;
+use App\Seance;
+use App\Work;
+use App\Test;
+use Carbon\Carbon;
+
+class CalendarController extends Controller
+{
+    public function view() 
+    {
+        $title = "Planning";
+        if (\Auth::user()->status === 1) {
+            $courses = Course::where('teacher_id', '=', \Auth::user()->id)->get();
+            $seances = [];
+            $works = [];
+            $tests = [];
+            foreach ($courses as $course) {
+                $seances[] = $course->seances;
+            }
+
+            foreach ($seances as $seance) {
+                foreach ($seance as $theSeance) {
+                    $works[] = $theSeance->works;
+                }
+                foreach ($seance as $theSeance) {
+                    $tests[] = $theSeance->tests;
+                }
+            }
+        }
+        return view('pages/planning', compact( 'title', 'seances', 'works', 'test' ));
+    }
+}
