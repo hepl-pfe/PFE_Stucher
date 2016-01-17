69f5fd8eea7ac3f63325dc2a21acb2d9df7ba547
Loïc Parent <loic1994@hotmail.com>
2016-01-17 22:44:27 +0100

    Create a file for changing profil picture

--- resources/views/pages/changePicture.blade.php
+++ resources/views/pages/changePicture.blade.php
@@ -1,3 +1,23 @@
+@extends('layout')
+@section('title', $title)
+@section('content')
+
+	<h2>{{ $title }}</h2>
+	<img src="{{ url() }}/img/profilPicture/{{ Auth::user()->image }}" alt="Image de profil">
+	<form action="" method="post" enctype="multipart/form-data">
+		<div class="form-group">
+			<input type="hidden" name="_token" value="{{ csrf_token() }}">
+			<label for="image">Changer l’image</label>
+			<input type="file" class="form-control" name="image" id="image">
+		</div>
+
+		<div class="form-group text-center">
+			<input type="submit" class="btn btn-primary" value="Valider les modifications">
+			<a href="{{ action( 'PageController@about' ) }}" class="btn btn-warning">Annuler</a>
+		</div>
+	</form>
 
 	@include('errors.profilError')
 
+
+@endsection
