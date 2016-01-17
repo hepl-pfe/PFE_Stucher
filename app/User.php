279ee135465d4078398fda244d7ff03a8a580303
Loïc Parent <loic1994@hotmail.com>
2016-01-17 16:11:48 +0100

    Add image and first name

--- app/User.php
+++ app/User.php
@@ -28,7 +28,7 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password', 'status'];
+    protected $fillable = ['firstname', 'name', 'image', 'email', 'status'];
 
     /**
      * The attributes excluded from the model's JSON form.
