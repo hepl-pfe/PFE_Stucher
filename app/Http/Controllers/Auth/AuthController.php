8d65f5e22a24b0c9d3abc0e61ee9e27cbc6f6cef
Loïc Parent <loic1994@hotmail.com>
2016-01-17 16:10:24 +0100

    Add first name

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -47,6 +47,7 @@ class AuthController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
+            'firstname' => 'required|max:255',
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|confirmed|min:6',
@@ -63,6 +64,7 @@ class AuthController extends Controller
     protected function create(array $data)
     {
         return User::create([
+            'firstname' => $data['firstname'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
