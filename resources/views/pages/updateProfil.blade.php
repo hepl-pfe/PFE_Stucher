f2fd361cdc019d42e4b3be3df1fd78a3d91ac27f
Loïc Parent <loic1994@hotmail.com>
2016-01-17 15:59:56 +0100

    Add first name and password

--- resources/views/pages/updateProfil.blade.php
+++ resources/views/pages/updateProfil.blade.php
@@ -5,6 +5,11 @@
 	<h2>{{ $title }}</h2>
 	<form action="" method="post">
 		<div class="form-group">
+			<label for="firstname">Votre prénom</label>
+			<input type="text" class="form-control" name="firstname" id="firstname" placeholder="ex: Mathématiques" value="{{ $firstname }}">
+		</div>
+		
+		<div class="form-group">
 			<input type="hidden" name="_token" value="{{ csrf_token() }}">
 			<label for="name">Votre nom</label>
 			<input type="text" class="form-control" name="name" id="name" placeholder="ex: Mathématiques" value="{{ $name }}">
@@ -17,12 +22,12 @@
 
 		<div class="form-group">
 			<label for="password">votre nouveau mot de passe</label>
-			<input type="text" class="form-control" name="password" id="password">
+			<input type="password" class="form-control" name="password" id="password" value="{{ $password }}">
 		</div>
 		
 		<div class="form-group">
-			<label for="chackPassword">valider le nouveau mot de passe</label>
-			<input type="text" class="form-control" name="chackPassword" id="chackPassword">
+			<label for="checkPassword">valider le nouveau mot de passe</label>
+			<input type="password" class="form-control" name="checkPassword" id="checkPassword" value="{{ $password }}">
 		</div>
 
 		<div class="form-group text-center">
@@ -30,6 +35,8 @@
 			<a href="{{ action( 'PageController@about', [ 'id' => $id ] ) }}" class="btn btn-warning">Annuler</a>
 		</div>
 	</form>
+
+	@include('errors.profilError')
 
 
 @endsection
