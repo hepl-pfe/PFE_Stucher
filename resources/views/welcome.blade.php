0059354c8905c1e693194bb704f6fd322c0a0520
Loïc Parent <loic1994@hotmail.com>
2016-04-30 18:11:45 +0200

    Change the favicon and add a new title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,24 +2,28 @@
 <html lang="fr-BE">
 <head>
     <meta charset="UTF-8">
-    <title>Accueil</title>
+    <title>Stucher</title>
     {{-- Fav icon --}}
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ url() }}/img/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ url() }}/img/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ url() }}/img/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ url() }}/img/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ url() }}/img/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ url() }}/img/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="{{ url() }}/img/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ url() }}/img/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ url() }}/img/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="{{ url() }}/img/favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ url() }}/img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ url() }}/img/favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ url() }}/img/favicon/favicon-16x16.png">
-    <link rel="manifest" href="{{ url() }}/img/favicon/manifest.json">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ url() }}/img/favicons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ url() }}/img/favicons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ url() }}/img/favicons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ url() }}/img/favicons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ url() }}/img/favicons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ url() }}/img/favicons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ url() }}/img/favicons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ url() }}/img/favicons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url() }}/img/favicons/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="{{ url() }}/img/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="{{ url() }}/img/favicons/favicon-194x194.png" sizes="194x194">
+    <link rel="icon" type="image/png" href="{{ url() }}/img/favicons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="{{ url() }}/img/favicons/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="{{ url() }}/img/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="{{ url() }}/img/favicons/manifest.json">
+    <link rel="mask-icon" href="{{ url() }}/img/favicons/safari-pinned-tab.svg" color="#ff4732">
+    <meta name="apple-mobile-web-app-title" content="Stucher">
+    <meta name="application-name" content="Stucher">
+    <meta name="msapplication-TileColor" content="#ff4732">
+    <meta name="msapplication-TileImage" content="{{ url() }}/img/favicons/mstile-144x144.png">
     <meta name="theme-color" content="#ffffff">
 
     
