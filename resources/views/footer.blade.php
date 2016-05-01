614f4790dab53ccf3a9790d0679d7477e7245db9
Loïc Parent <loic1994@hotmail.com>
2016-05-01 23:52:05 +0200

    Add a new part file to include for the footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+    <!--[if lte IE 7]><script src="icons-lte-ie7.js"></script><![endif]-->
+    <script src="{{ url() }}/js/jquery.js"></script>
+    <script src="{{ url() }}/js/jquery-ui.js"></script>
+    <script src="{{ url() }}/js/main.js"></script>
+    </body>
+</html>
