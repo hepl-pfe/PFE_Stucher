70653ecda661b75eff0a0077a67d4ec6e95567c7
Loïc Parent <loic1994@hotmail.com>
2016-05-05 19:07:24 +0200

    change layout
    => show number of notification not seen
    => icons
    => notifications

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,69 +9,82 @@
 		<nav class="mainNav">
 			<h2 class="hidden">Menu principale</h2>
 			<ul class="mainNav__ul">
-					<li><a class="<?php if ( isset($activePage) && $activePage == 'profil' ) { echo 'active';} ?>" href="{!! action( 'PageController@about' ) !!}">{{ Auth::user()->firstname }} {{ Auth::user()->name }}</a></li>
-					<li><a class="<?php if ( isset($activePage) && $activePage == 'course' ) { echo 'active';} ?>" href="{!! action( 'CourseController@index' ) !!}">Mes cours</a></li>
-					<li><a class="<?php if ( isset($activePage) && $activePage == 'notification' ) { echo 'active';} ?>" href="{!! action( 'NotificationController@index' ) !!}">Notifications</a></li>
+					<li><a class="<?php if ( isset($activePage) && $activePage == 'profil' ) { echo 'active';} ?>" href="{!! action( 'PageController@about' ) !!}"><span class="icon-user unlink whiteText whiteText--icon"></span>{{ Auth::user()->firstname }} {{ Auth::user()->name }}</a></li>
+					<li><a class="<?php if ( isset($activePage) && $activePage == 'course' ) { echo 'active';} ?>" href="{!! action( 'CourseController@index' ) !!}">
+							@if( \Auth::user()->status == 1 )
+							<span class="icon-briefcase unlink whiteText whiteText--icon"></span>
+							@else
+							<span class="icon-graduation unlink whiteText whiteText--icon"></span>
+							@endif
+							Mes cours</a>
+					</li>
+					<li><a class="<?php if ( isset($activePage) && $activePage == 'notification' ) { echo 'active';} ?>" href="{!! action( 'NotificationController@index' ) !!}"><span class="icon-bell unlink whiteText whiteText--icon"></span>Notifications
+							<span class="notificationNumer--menu">
+								@if ( count($notifications) != 'null' )
+									({{ count($notifications) }})
+								@endif
+							</span></a></li>
 					<!-- <li><a class="<?php //if ( isset($activePage) && $activePage == 'message' ) { echo 'active';} ?>"href="{!! action( 'PageController@message' ) !!}">Messages</a></li> -->
-					<li><a class="<?php if ( isset($activePage) && $activePage == 'planning' ) { echo 'active';} ?>" href="{!! action( 'CalendarController@view' ) !!}">Mon planning</a></li>
-					<li><a href="{!! action( 'Auth\AuthController@getLogout' ) !!}">Se déconnecter</a></li>
+					<li><a class="<?php if ( isset($activePage) && $activePage == 'planning' ) { echo 'active';} ?>" href="{!! action( 'CalendarController@view' ) !!}"><span class="icon-calendar unlink whiteText whiteText--icon"></span>Mon planning</a></li>
+					<li><a href="{!! action( 'Auth\AuthController@getLogout' ) !!}"><span class="icon-power unlink whiteText whiteText--icon"></span>Se déconnecter</a></li>
 			</ul>
 		</nav>
 
-		<div class="notification__group--side">
+		<div class="notification__group__header">
 			<h2 class="notification__title">NOTIFICATIONS</h2>
-			<ul class="notification__group--side--list">
+			<ul class="notification__group--box">
 				@if ( count($notifications) != 'null' )
 					@foreach ($notifications as $not)
-						@if ($not->not_context == 1)
+						<li class="notification__item">
+							@if ($not->not_context == 1)
 
-						@endif
+							@endif
 
-						@if ($not->not_context == 2)
+							@if ($not->not_context == 2)
 
-						@endif
+							@endif
 
-						@if ($not->not_context == 3)
+							@if ($not->not_context == 3)
 
-						@endif
+							@endif
 
-						@if ($not->not_context == 4)
+							@if ($not->not_context == 4)
 
-						@endif
+							@endif
 
-						@if ($not->not_context == 5)
-							<li class="notification__group--item">
-								<a href="{{ action('PageController@viewUser', [ 'id' => $not->user_id]) }}">{{$not->user_name}}</a> {{$not->not_title}} <a href="{{ action('CourseController@view', [ 'id' => $not->course_id, 'action' => 1 ]) }}">{{$not->course_title}}</a>
+							@if ($not->not_context == 5)
+								<div class="notification__content notification__color--green">
+									<a href="{{ action('PageController@viewUser', [ 'id' => $not->user_id]) }}"> {{$not->user_firstname}} {{$not->user_name}}</a> Demande accès au cours de <a href="{{ action('CourseController@view', [ 'id' => $not->course_id, 'action' => 1 ]) }}">{{$not->course_title}}</a>
+								</div>
 								@if ($not->not_seen != 3)
-									<a class="btn btn-success pull-right" href="{!! action( 'CourseController@acceptStudent', ['id_course' => $not->course_id, 'id_user' => $not->user_id] ) !!}">Ajouter</a> <a class="btn btn-danger pull-right" href="{!! action( 'CourseController@removeStudentFromCourse', ['id_course' => $not->course_id, 'id_user' => $not->user_id] ) !!}">Refuser</a>
+									<div class="notification__actionGroup double">
+										<a class="notification__button icon unlink success" href="{!! action( 'CourseController@acceptStudent', ['id_course' => $not->course_id, 'id_user' => $not->user_id] ) !!}"><span class="icon-check"></span><span class="hidden">Ajouter</span></a>
+
+										<a class="notification__button icon unlink danger" href="{!! action( 'CourseController@removeStudentFromCourse', ['id_course' => $not->course_id, 'id_user' => $not->user_id] ) !!}"><span class="icon-close"></span><span class="hidden">Refuser</span></a>
+									</div>
 								@endif
-							</li>
-						@endif
+							@endif
 
-						@if ($not->not_context == 6)
-							<li class="notification__group--item">
+							@if ($not->not_context == 6)
 								{{$not->not_title}} <a href="{{ action('CourseController@view', [ 'id' => $not->course_id, 'action' => 1 ]) }}">{{$not->course_title}}</a>
-							</li>
-						@endif
+							@endif
 
-						@if ($not->not_context == 7)
-							<li class="notification__group--item">
+							@if ($not->not_context == 7)
 								{{$not->not_title}} {{$not->course_title}}
-							</li>
-						@endif
+							@endif
 
-						@if ($not->not_context == 8)
-							<li class="notification__group--item">
+							@if ($not->not_context == 8)
 								<a href="{{ action('PageController@viewUser', [ 'id' => $not->user_id]) }}">{{$not->user_name}}</a> {{$not->not_title}} <a href="{{ action('CourseController@view', [ 'id' => $not->course_id, 'action' => 1 ]) }}">{{$not->course_title}}</a>
-							</li>
-						@endif
+							@endif
+							<div class="clear"></div>
+						</li>
 					@endforeach
 				@else
-					<li class="notification__group--item--null">
+					<li class="notification__item--null">
 						Aucune notification pour le moment
 					</li>
 				@endif
-				<li class="notification__group--item--button"><a href="{!! action( 'NotificationController@index' ) !!}">Afficher toutes les news</a></li>
+				<li class="notification__item--more"><a href="{!! action( 'NotificationController@index' ) !!}">Afficher toutes les news</a></li>
 			</ul>
 		</div>
 	</div>
