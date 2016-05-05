1c615bd12b35ad80299d9e90a66c33065412369b
Loïc Parent <loic1994@hotmail.com>
2016-05-05 19:06:10 +0200

    change footer
    => add new script

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,10 @@
     <!--[if lte IE 7]><script src="icons-lte-ie7.js"></script><![endif]-->
     <script src="{{ url() }}/js/jquery.js"></script>
-    <script src="{{ url() }}/js/jquery-ui.js"></script>
+    {{--<script src="{{ url() }}/js/jquery-ui.js"></script>--}}
+    <!-- Candendar popup -->
+    <script type="text/javascript" src="{{ url() }}/calendar-popup/jquery.timepicker.min.js"></script>
+    <script type="text/javascript" src="{{ url() }}/calendar-popup/lib/bootstrap-datepicker.js"></script>
+    <!-- My scripts -->
     <script src="{{ url() }}/js/main.js"></script>
     </body>
 </html>
