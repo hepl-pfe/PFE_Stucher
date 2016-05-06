c9b2a64c22f497be931e0dece9fe39a2156a6151
Loïc Parent <loic1994@hotmail.com>
2016-05-06 20:33:46 +0200

    show the item if teacher

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -14,7 +14,9 @@
 		<label for="dd_moreButton" class="dd_moreButton--button"><span></span><span></span></label>
 
 		<ul class="dd_moreButton--content">
-			<li><a href="{!! action( 'CourseController@create' ) !!}">Créer un cours</a></li>
+			@if( Auth::user()->status == 1 )
+				<li><a href="{!! action( 'CourseController@create' ) !!}">Créer un cours</a></li>
+			@endif
 			<li><a href="{!! action( 'PageController@editProfil' ) !!}">Modifier mon profil</a></li>
 			<li><a href="{{ action( 'PageController@changePicture' ) }}">Changer la photo de profil</a></li>
 			<li><a href="{!! action( 'PageController@deleteProfil' ) !!}">Supprimer mon compte</a></li>
