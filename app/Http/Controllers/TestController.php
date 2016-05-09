bd032f2a963af38c914ba7bbc75937509a7ab801
Loïc Parent <loic1994@hotmail.com>
2016-05-09 21:39:36 +0200

    change for server

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -71,7 +71,7 @@ class TestController extends Controller
                     $nameParts = explode('.', $fileName);
                     $ext = strtolower(end($nameParts));
 
-                    if ( $ext === 'jpeg' OR $ext === 'gif' OR $ext === 'png' OR $ext === 'txt' OR $ext === 'pdf' OR $ext === 'docx' OR $ext === 'doc' ) {
+                    if ( $ext == 'jpeg' OR $ext == 'gif' OR $ext == 'png' OR $ext == 'txt' OR $ext == 'pdf' OR $ext == 'docx' OR $ext == 'doc' ) {
                         // compléter la liste au fur et à mesure
 
                         $type = $ext;
@@ -147,7 +147,7 @@ class TestController extends Controller
                     $nameParts = explode('.', $fileName);
                     $ext = strtolower(end($nameParts));
 
-                    if ( $ext === 'jpeg' OR $ext === 'gif' OR $ext === 'png' OR $ext === 'txt' OR $ext === 'pdf' OR $ext === 'docx' OR $ext === 'doc' ) {
+                    if ( $ext == 'jpeg' OR $ext == 'gif' OR $ext == 'png' OR $ext == 'txt' OR $ext == 'pdf' OR $ext == 'docx' OR $ext == 'doc' ) {
                         // compléter la liste au fur et à mesure
 
                         $type = $ext;
