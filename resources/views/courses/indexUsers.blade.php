18657245fc6a55d55f7c23e4b11f59951eca2108
Loïc Parent <loic1994@hotmail.com>
2016-05-11 14:25:01 +0200

    check if teacher
    => show/hide dropdown menu
    => show/hide down remove button

--- resources/views/courses/indexUsers.blade.php
+++ resources/views/courses/indexUsers.blade.php
@@ -7,14 +7,16 @@
 </div>
 
 <!-- dd_moreButton -->
-<div class="dd_moreButton">
-	<input type="checkbox" id="dd_moreButton">
-	<label for="dd_moreButton" class="dd_moreButton--button"><span></span><span></span></label>
+@if( \Auth::user()->status == 1 )
+	<div class="dd_moreButton">
+		<input type="checkbox" id="dd_moreButton">
+		<label for="dd_moreButton" class="dd_moreButton--button"><span></span><span></span></label>
 
-	<ul class="dd_moreButton--content">
-		<li><a href="{!! action( 'CourseController@create' ) !!}">Ajouter un cours</a></li>
-	</ul>
-</div>
+		<ul class="dd_moreButton--content">
+			<li><a href="{!! action( 'CourseController@create' ) !!}">Ajouter un cours</a></li>
+		</ul>
+	</div>
+@endif
 
 <div class="box--group">
 	<div class="box box--shadow">
@@ -23,7 +25,9 @@
 			@foreach( $inCourseStudents as $student )
 				<li class="groupList__type1">
 					<a href="{{ action( 'PageController@viewUser', [ 'id' => $student->id ] ) }}">{{ $student->firstname }} {{ $student->name }}</a>
+					@if( \Auth::user()->status == 1 )
 					<a class="seance__item--button delete" href="{!! action( 'CourseController@removeStudentFromCourse', ['id_course' => $course->id, 'id_user' => $student->id] ) !!}">Retirer de ce cours</a>
+					@endif
 				</li>
 			@endforeach
 		</ul>
