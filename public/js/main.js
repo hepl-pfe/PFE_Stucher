ee34b2495d27872240f9813cc327456b4e92fb07
Loïc Parent <loic1994@hotmail.com>
2016-05-17 12:24:26 +0200

    popup on delete Test

--- public/js/main.js
+++ public/js/main.js
@@ -347,6 +347,47 @@ jQuery( function($) {
 		});
 
 	} );
+
+
+
+
+	$( '.action__deleteTest').click( function( e ){
+		e.preventDefault();
+		var profil_url = e.currentTarget.href;
+		var seance = e.currentTarget.getAttribute('data-seance');
+
+		swal({
+			title: "Voulez vous vraiment supprimer cette interrogation?",
+			text: "En supprimant cette interrogation, vous supprimerez tous les fichiers liés à celle-ci",
+			showCancelButton: true,
+			closeOnConfirm: false,
+			confirmButtonText: "Oui, je supprime",
+			cancelButtonText: "Non",
+			confirmButtonColor: "#ec6c62"
+		}, function() {
+			$.ajax({
+					type: "get",
+					url: profil_url
+				})
+				.done(function(data) {
+					swal({
+						title: "Supprimé!",
+						text: "L’interrogation a correctement été supprimée!",
+						type: "success"
+					}, function() {
+						window.location.href = '/seance/'+seance+'/view';
+					});
+				})
+				.error(function(data) {
+					swal("Oops", "Une erreur s'est produite sur le serveur!", "error");
+				});
+		});
+
+	} );
+
+
+
+
 	// IF ACCESS ERROR
 	function getquerystringParams () {
 		var str = document.location.search;
