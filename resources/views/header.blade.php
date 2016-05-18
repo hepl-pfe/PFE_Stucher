b30dafb743c3ff268f8fbf3a6d54e2eb5b524bd7
Loïc Parent <loic1994@hotmail.com>
2016-05-18 11:00:25 +0200

    add new styles files and precise the type

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -46,8 +46,13 @@
     {{--<link rel="stylesheet" href="{{ url() }}/css/jquery-ui.css">
     <link rel="stylesheet" href="{{ url() }}/css/jquery-ui.theme.css">--}}
     <link href='https://fonts.googleapis.com/css?family=Asap:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="{{ url() }}/css/main.css">
+    <link rel="stylesheet" type="text/css" href="{{ url() }}/css/main.css">
+
     <!-- Calendar popup -->
+    <link rel="stylesheet" type="text/css" href="{{ url() }}/css/DateTimePicker.css" />
+    <!--[if lt IE 9]>
+    <link rel="stylesheet" type="text/css" href="{{ url() }}/css/DateTimePicker-ltie9.css" />
+    <![endif]-->
 
     <!-- Sweet Alert -->
     <link rel="stylesheet" type="text/css" href="{{ url() }}/css/sweetalert.css" />
