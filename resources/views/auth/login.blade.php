b4b2e49a5c2f449f33c7dc9466c832719faa466d
Loïc Parent <loic1994@hotmail.com>
2016-05-18 23:39:43 +0200

    Change the login and register files
    => add new design
    => change forms
    => add buttons
    => import header and footer
    => …

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,41 @@
-@extends('logoutLayout')
-@section('title', 'Connexion - Stucher')
-@section('content')
+@section('title', 'Inscription • Stucher')
+@include( 'header' )
+<body class="home default">
 
     <div class="blockTitle">
         <h2 class="mainTitle">Connexion</h2>
+        <a title="Revenir à la page d’accueil" class="backButton blockTitle__backButton unlink mainColorfont" href="{!! route( 'home' ) !!}"><span class="hidden">Revenir à la page précédente</span><span class="icon-arrow-left"></span></a>
     </div>
 
-    <form method="POST" action="/auth/login">
-        {!! csrf_field() !!}
+    <div class="box--group">
+        <div class="box box--shadow">
+            <form class="box__group--content" method="POST" action="/login">
+                {!! csrf_field() !!}
 
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" id="email" value="{{ old('email') }}">
-        </div>
+                <div class="form-group">
+                    <label class="color_label" for="email">Email</label>
+                    <input class="form-control" type="email" name="email" id="email" value="{{ old('email') }}" autofocus>
+                </div>
 
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input class="form-control" type="password" name="password" id="password">
-        </div>
+                <div class="form-group">
+                    <label class="color_label" for="password">Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
 
-        <div class="form-group">
-            <input type="checkbox" name="remember"> Remember Me
-        </div>
+                <div class="form-group">
+                    <input class="form__remember" type="checkbox" checked id="remember" name="remember"> <label for="remember">Se souvenir de moi</label>
 
-        <div class="form-group">
-            <button class="btn btn-default" type="submit">Login</button>
+                    <a class="unlink forgetPassword" href="{{ action( 'Auth\PasswordController@getEmail' ) }}">J'ai oublié mon mot de passe</a>
+                </div>
+
+                <div class="form-group--button">
+                    <button class="btn btn-send" type="submit">Se connecter</button>
+                    <div class="clear"></div>
+                </div>
+
+                @include( 'errors.profilError' )
+            </form>
         </div>
-        
-    </form>
-@stop
+    </div>
+
+@include( 'footer' )
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,45 +1,59 @@
-@extends('logoutLayout')
-@section('title', 'Inscription')
-@section('content')
+@section('title', 'Inscription • Stucher')
+@include( 'header' )
+<body class="home default">
+
     <div class="blockTitle">
         <h2 class="mainTitle">Inscription</h2>
+        <a title="Revenir à la page d’accueil" class="backButton blockTitle__backButton unlink mainColorfont" href="{!! route( 'home' ) !!}"><span class="hidden">Revenir à la page précédente</span><span class="icon-arrow-left"></span></a>
     </div>
 
-    <form method="POST" action="/auth/register">
-        {!! csrf_field() !!}
+    <div class="box--group">
+        <div class="box box--shadow">
+            <form class="box__group--content" method="POST" action="/register">
+                {!! csrf_field() !!}
 
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input class="form-control" type="text" name="name" id="name" value="{{ old('name') }}">
-                <!-- old()  => est une variable temporaire qui récupère (le temps d'une requete) les information SANS le mot de passe histoire d’éviter de devoir TOUT retapper. -->
+                <div class="form-group">
+                    <label class="color_label" for="firstname">Prénom</label>
+                    <input class="form-control" type="text" name="firstname" id="firstname" value="{{ old('firstname') }}" autofocus>
+                </div>
+
+                <div class="form-group">
+                    <label class="color_label" for="name">Nom</label>
+                    <input class="form-control" type="text" name="name" id="name" value="{{ old('name') }}">
+                </div>
+
+                <div class="form-group">
+                    <label class="color_label" for="email">Email</label>
+                    <input class="form-control" type="email" name="email" id="email" value="{{ old('email') }}">
+                </div>
+
+                <div class="form-group">
+                    <label class="color_label" for="password">Mot de passe</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+
+                <div class="form-group">
+                    <label class="color_label" for="password_confirmation">Confirmation du mot de passe</label>
+                    <input class="form-control" type="password" name="password_confirmation" id="password_confirmation">
+                </div>
+
+                <div class="form-group">
+                    <p>Vous êtes…</p>
+                    <label for="teacher">Un professeur</label>
+                    <input class="form-control" type="radio" name="status" id="teacher" value="1">
+
+                    <label for="student">Un étudiant</label>
+                    <input class="form-control" type="radio" name="status" id="student" value="2">
+                </div>
+
+                <div class="form-group--button">
+                    <button class="btn btn-send" type="submit">S'inscrire</button>
+                    <div class="clear"></div>
+                </div>
+
+                @include( 'errors.profilError' )
+            </form>
         </div>
+    </div>
 
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" id="email" value="{{ old('email') }}">
-        </div>
-
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input class="form-control" type="password" name="password" id="password">
-        </div>
-
-        <div class="form-group">
-            <label for="password_confirmation">Confirm Password</label>
-            <input class="form-control" type="password" name="password_confirmation" id="password_confirmation">
-        </div>
-
-        <div class="form-group">
-            <p>Vous êtes…</p>
-            <label for="teacher">Un professeur</label>
-            <input class="form-control" type="radio" name="status" id="teacher" value="1"> 
-
-            <label for="student">Un étudiant</label>
-            <input class="form-control" type="radio" name="status" id="student" value="2">
-        </div>
-
-        <div class="form-group">
-            <button class="btn btn-default" type="submit">Register</button>
-        </div>
-    </form>
-@stop
+@include( 'footer' )
