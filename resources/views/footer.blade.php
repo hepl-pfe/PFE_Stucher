52ad95a4899ff5ad1e82f9acab16521019f109c2
Loïc Parent <loic1994@hotmail.com>
2016-05-18 10:59:47 +0200

    Add new scripts

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,9 +2,16 @@
     <script src="{{ url() }}/js/jquery.js"></script>
 
     <!-- Candendar popup -->
+    <script type="text/javascript" src="{{ url() }}/js/DateTimePicker.js"></script>
+    <!--[if lt IE 9]>
+    <script type="text/javascript" src="{{ url() }}/js/DateTimePicker-ltie9.js"></script>
+    <![endif]-->
+
     <!-- Sweet Alert -->
     <script type="text/javascript" src="{{ url() }}/js/sweetalert.min.js"></script>
+
     <!-- My scripts -->
     <script src="{{ url() }}/js/main.js"></script>
+
     </body>
 </html>
