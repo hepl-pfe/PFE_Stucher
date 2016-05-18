a53b4383e39c753ce8b298f4d71eda4511aee68b
Loïc Parent <loic1994@hotmail.com>
2016-05-18 10:59:36 +0200

    remove useless script

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,9 +1,7 @@
     <!--[if lte IE 7]><script src="icons-lte-ie7.js"></script><![endif]-->
     <script src="{{ url() }}/js/jquery.js"></script>
-    {{--<script src="{{ url() }}/js/jquery-ui.js"></script>--}}
+
     <!-- Candendar popup -->
-    <script type="text/javascript" src="{{ url() }}/calendar-popup/jquery.timepicker.min.js"></script>
-    <script type="text/javascript" src="{{ url() }}/calendar-popup/lib/bootstrap-datepicker.js"></script>
     <!-- Sweet Alert -->
     <script type="text/javascript" src="{{ url() }}/js/sweetalert.min.js"></script>
     <!-- My scripts -->
