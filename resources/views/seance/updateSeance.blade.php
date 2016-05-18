39503c2fc43fc6d1404433a19c1833c58cc99086
Loïc Parent <loic1994@hotmail.com>
2016-05-18 11:01:29 +0200

    Change times input

--- resources/views/seance/updateSeance.blade.php
+++ resources/views/seance/updateSeance.blade.php
@@ -24,12 +24,12 @@
 
 			<div class="form-group">
 				<label for="start_hours">heure de début</label>
-				<input type="time" class="form-control" name="start_hours" id="start_hours" value="{{ $start_hours }}">
+				<input type="time" class="start_hours" name="start_hours" id="start_hours" value="{{ $start_hours }}" data-field="time" data-startend="start" data-format="hh:mm" data-startendelem=".end_hours" readonly>
 			</div>
 
 			<div class="form-group">
 				<label for="end_hours">heure de fin</label>
-				<input type="time" class="form-control" name="end_hours" id="end_hours" value="{{ $end_hours }}">
+				<input type="time" class="end_hours" name="end_hours" id="end_hours" value="{{ $end_hours }}" data-field="time" data-format="hh:mm" data-startend="end" data-startendelem=".start_hours" readonly>
 			</div>
 
 			<div class="form-group text-center">
