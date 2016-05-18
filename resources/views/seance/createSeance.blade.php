7b1d0d3188f0d00cf57bb6fc4b2e04bbfb9fbe4f
Loïc Parent <loic1994@hotmail.com>
2016-05-18 11:04:03 +0200

    Change this file
    => add calendar popup
    => change inputs
    => remove choice for course to simplify user experience
    => …

--- resources/views/seance/createSeance.blade.php
+++ resources/views/seance/createSeance.blade.php
@@ -10,13 +10,9 @@
 		<form action="" method="post">
 			<div class="form-group">
 				<input type="hidden" name="_token" value="{{ csrf_token() }}">
-				<label for="course">Pour le cours de…</label>
-				<select class="form-control" name="course" id="title">
-	            	@foreach( $courses as $course )
-		            	<option @if($course->id == $id)selected="selected" @endif value="{{ $course->id }}">{{ $course->title }}</option>
-	            	@endforeach
-	            </select>
-			
+				<input type="hidden" name="course" value="{{ $id }}">
+			</div>
+
 			<div class="form-group">
 				<label for="daypicker">Pour quel jour</label>
 				<select name="daypicker" id="daypicker">
@@ -26,25 +22,24 @@
 				</select>
 			</div>
 
-			<fieldset>
-				<div class="form-group">
-					<label for="datepicker">Début de période</label>
-					<input type="date" class="form-control" name="start_date" id="datepicker_start" value="{{ $today }}">
-				</div>
-				<div class="form-group">
-					<label for="datepicker">Fin de période</label>
-					<input type="date" class="form-control" name="end_date" id="datepicker_end" value="{{ $tomorrow }}">
-				</div>
-			</fieldset>
+			<div class="form-group">
+				<label for="datepicker_start">Début de période</label>
+				<input type="date" class="start_date" name="start_date" id="datepicker_start" data-field="date" data-startend="start" data-startendelem=".end_date" readonly value="{{ $today }}">
+			</div>
+
+			<div class="form-group">
+				<label for="datepicker_end">Fin de période</label>
+				<input type="date" class="end_date" name="end_date" id="datepicker_end" data-field="date" data-startend="end" data-startendelem=".start_date" readonly placeholder="{{ $tomorrow }}">
+			</div>
 
 			<div class="form-group">
 				<label for="start_hours">heure de début</label>
-				<input type="time" class="form-control" name="start_hours" id="start_hours" value="08:00">
+				<input type="time" class="start_hours" name="start_hours" id="start_hours" value="08:00" data-field="time" data-startend="start" data-format="hh:mm" data-startendelem=".end_hours" readonly>
 			</div>
 
 			<div class="form-group">
 				<label for="end_hours">heure de fin</label>
-				<input type="time" class="form-control" name="end_hours" id="end_hours" value="10:00">
+				<input type="time" class="end_hours" name="end_hours" id="end_hours" placeholder="10:00" data-field="time" data-format="hh:mm" data-startend="end" data-startendelem=".start_hours" readonly>
 			</div>
 
 			<div class="form-group text-center">
@@ -52,6 +47,9 @@
 				<input type="submit" class="btn btn-send" value="Générer les séances">
 				<div class="clear"></div>
 			</div>
+
+			<div id="dtBox"></div>
+
 			@include( 'errors.profilError' )
 		</form>
 	</div>
