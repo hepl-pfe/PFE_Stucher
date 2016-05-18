db485b2d12a700ec003e0cfc0d58378edf68ec5c
Loïc Parent <loic1994@hotmail.com>
2016-05-18 23:38:44 +0200

    Change routes for login and registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -104,13 +104,13 @@ Route::get( 'registerStudent', [ 'as' => 'registerStudent', 'uses' => 'PageContr
 Route::get( 'registerTeacher', [ 'as' => 'registerTeacher', 'uses' => 'PageController@registerTeacher' ] );
 
 // Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('login', 'Auth\AuthController@getLogin');
+Route::post('login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 // Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+Route::get('register', 'Auth\AuthController@getRegister');
+Route::post('register', 'Auth\AuthController@postRegister');
 
 
 // profilPicture routes…
