0cb594fa5f2e66650ae608e36697f1f9df123aef
Loïc Parent <loic1994@hotmail.com>
2016-05-18 11:01:00 +0200

    Add a link mailto

--- resources/views/pages/AllMystudents.blade.php
+++ resources/views/pages/AllMystudents.blade.php
@@ -9,7 +9,9 @@
 @if ( count($students) !== 0 )
 	<ul class="panel-body">
 		@foreach( $students as $student )
-			<li><a href="{{ action( 'PageController@viewUser', [ 'id' => $student[0]->id ] ) }}">{{ $student[0]->firstname }} {{ $student[0]->name }}</a>
+			<li>
+				<a href="{{ action( 'PageController@viewUser', [ 'id' => $student[0]->id ] ) }}">{{ $student[0]->firstname }} {{ $student[0]->name }}</a>
+				<a href="mailto:{{ $student[0]->email}}">Contactez {{ $student[0]->firstname }}</a>
 		@endforeach
 	</ul>
 @else
