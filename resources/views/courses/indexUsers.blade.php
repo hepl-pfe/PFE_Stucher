c85bde0a6e1181ddcff93fa5d63fe08e4da6383c
Loïc Parent <loic1994@hotmail.com>
2016-05-19 13:36:56 +0200

    Remove old elements

--- resources/views/courses/indexUsers.blade.php
+++ resources/views/courses/indexUsers.blade.php
@@ -19,23 +19,6 @@
 	</div>
 @endif
 
-<div class="box--group">
-	<div class="box box--shadow">
-		@if ( count($inCourseStudents) !== 0 )
-		<ul class="list__course_box--group">
-			@foreach( $inCourseStudents as $student )
-				<li class="groupList__type1">
-					<a href="{{ action( 'PageController@viewUser', [ 'id' => $student->id ] ) }}">{{ $student->firstname }} {{ $student->name }}</a>
-					@if( \Auth::user()->status == 1 )
-					<a class="seance__item--button delete" href="{!! action( 'CourseController@removeStudentFromCourse', ['id_course' => $course->id, 'id_user' => $student->id] ) !!}">Retirer de ce cours</a>
-					@endif
-				</li>
-			@endforeach
-		</ul>
-		@else
-			<p class="center">Il n’y a aucun étudiant pour le moment</p>
-		@endif
 	</div>
-</div>
 
 @stop
