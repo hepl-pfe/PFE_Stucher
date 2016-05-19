6eb6aba1bbd6920df8825563cde4f5bfc20257cf
Loïc Parent <loic1994@hotmail.com>
2016-05-19 13:36:22 +0200

    Add classes and change Text

--- resources/views/courses/createCourse.blade.php
+++ resources/views/courses/createCourse.blade.php
@@ -11,22 +11,22 @@
 			<form class="box__group--content" action="" method="post">
 				<div class="form-group">
 					<input type="hidden" name="_token" value="{{ csrf_token() }}">
-					<label for="title">Ajouter le titre du cours</label>
+					<label class="color_label" for="title">Intitulé du cours</label>
 					<input type="text" class="form-control" name="title" id="title" placeholder="ex: Mathématiques" value="{{ old('title') }}" autofocus>
 				</div>
 
 				<div class="form-group">
-					<label for="group">Ajouter le nom du groupe</label>
+					<label class="color_label" for="group">Nom du groupe</label>
 					<input type="text" class="form-control" name="group" id="group" placeholder="ex: 3e gestion" value="{{ old('group') }}">
 				</div>
 
 				<div class="form-group">
-					<label for="school">Ajouter le nom de l'établissement scolaire</label>
+					<label class="color_label" for="school">Établissement scolaire</label>
 					<input type="text" class="form-control" name="school" id="school" placeholder="ex: Saint-luc" value="{{ old('school') }}">
 				</div>
 
 				<div class="form-group">
-					<label for="place">Ajouter le nom de la ville où se trouve cette école</label>
+					<label class="color_label" for="place">Ville de cette école</label>
 					<input type="text" class="form-control" name="place" id="place" placeholder="ex: Liège" value="{{ old('place') }}">
 				</div>
 
