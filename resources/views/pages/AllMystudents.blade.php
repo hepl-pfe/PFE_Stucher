7b095e6e2db9bc4796cb0badad6e6f748550ae41
Loïc Parent <loic1994@hotmail.com>
2016-05-20 10:33:06 +0200

    Design 2.0 + add actions
    => delete user
    => contact user

--- resources/views/pages/AllMystudents.blade.php
+++ resources/views/pages/AllMystudents.blade.php
@@ -11,12 +11,24 @@
 		<div class="box box--shadow box--studentAll">
 			<ul>
 				@foreach ($students as $student)
-					<li class="box__group--list--list box__group--studentAsk">
-						<a class="profilPicName" href="{{ action( 'PageController@viewUser', [ 'id' => $student[0]->id ] ) }}">
+					<li class="box__group--list--list box__group--students">
+						<a class="profilPicName list__users--middle" href="{{ action( 'PageController@viewUser', [ 'id' => $student[0]->id ] ) }}">
 							<img class="box__profilImage box__profilImage--small" src="{{ url() }}/img/profilPicture/{{ $student[0]->image }}" alt="Image de profil">
 							<span>{{ $student[0]->firstname }} {{$student[0]->name}}</span>
 						</a>
-						<a class="unlink box__list__rightButton" href="mailto:{{ $student[0]->email }}">Contacter</a>
+						<div class="box__list__rightButtons">
+							<a title="Contacter par mail" class="unlink box__list__rightButton" href="mailto:{{ $student[0]->email }}">
+								<span class="hidden">Contacter</span>
+								<span class="icon-envelope"></span>
+							</a>
+							@if( \Auth::user()->status == 1 )
+								<a title="Retirer cet élève de tout mes cours" class="unlink box__list__rightButton deleteButtonBg" href="{!! action( 'CourseController@removeStudent', ['id' => $student[0]->id] ) !!}">
+									<span class="hidden">Retirer de tout mes cours</span>
+									<span class="icon-trash"></span>
+								</a>
+							@endif
+							<div class="clear"></div>
+						</div>
 						<div class="clear"></div>
 					</li>
 				@endforeach
