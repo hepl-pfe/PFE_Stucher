23bbc34a3dea5dd0d414db355a99b1683807e5a7
Loïc Parent <loic1994@hotmail.com>
2016-05-20 10:32:16 +0200

    Add a title

--- resources/views/courses/waitCourse.blade.php
+++ resources/views/courses/waitCourse.blade.php
@@ -4,5 +4,6 @@
 <div class="blockTitle">
     <h2 class="mainTitle">{{ $title }}</h2>
 </div>
+    <h3>Cours en attente de validation</h3>
     <a class="btn btn-warning" href="{!! action( 'CourseController@searchCourse' ) !!}">Retour</a>
 @stop
