c3bba1caf3ee6161c64877615d4d617eb334d367
Loïc Parent <loic1994@hotmail.com>
2016-05-22 19:28:39 +0200

    Change the password reset page

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,31 +1,33 @@
-@extends('logoutLayout')
-@section('title', 'Mot de passe oublié')
-@section('content')
+@section('title', 'Mot de passe oublié • Stucher')
+@include( 'header' )
+<body class="home default">
+
 <!-- resources/views/auth/password.blade.php -->
 <div class="blockTitle">
     <h2 class="mainTitle">Mot de passe oublié</h2>
+    <a title="Revenir à la page précédente" class="backButton blockTitle__backButton unlink mainColorfont" href="{!! URL::previous() !!}"><span class="hidden">Revenir à la page précédente</span><span class="icon-arrow-left"></span></a>
 </div>
 
-<form method="POST" action="/password/email">
-    {!! csrf_field() !!}
 
-    @if (count($errors) > 0)
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    @endif
+<div class="box--group">
+    <div class="box box--shadow box__connect--page">
+        <form class="box__group--content" method="POST" action="/password/email">
+            {!! csrf_field() !!}
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
+            <div class="form-group">
+                <label class="color_label" for="email">Email</label>
+                <input class="form-control" type="email" name="email" id="email" value="{{ old('email') }}" autofocus>
+            </div>
+
+            <div class="form-group--button">
+                <button class="btn btn-send" type="submit">Envoyez un lien par email</button>
+                <div class="clear"></div>
+            </div>
+
+            @include( 'errors.profilError' )
+
+        </form>
     </div>
+</div>
 
-    <div>
-        <button type="submit">
-            Envoyez un lien par email
-        </button>
-    </div>
-</form>
-@stop
+@include( 'footer' )
