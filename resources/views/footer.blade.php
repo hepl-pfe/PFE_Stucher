ca098c5c1e5f462c7af4887c3a2618300e43e321
Loïc Parent <loic1994@hotmail.com>
2016-06-10 18:34:42 +0200

    replace scripts by minify scripts

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,17 +1,17 @@
-    <!--[if lte IE 7]><script src="icons-lte-ie7.js"></script><![endif]-->
+    <!--[if lte IE 7]><script src="icons-lte-ie7.min.js"></script><![endif]-->
     <script src="{{ url() }}/js/jquery.js"></script>
 
     <!-- Candendar popup -->
-    <script type="text/javascript" src="{{ url() }}/js/DateTimePicker.js"></script>
+    <script type="text/javascript" src="{{ url() }}/js/DateTimePicker.min.js"></script>
     <!--[if lt IE 9]>
-    <script type="text/javascript" src="{{ url() }}/js/DateTimePicker-ltie9.js"></script>
+    <script type="text/javascript" src="{{ url() }}/js/DateTimePicker-ltie9.min.js"></script>
     <![endif]-->
 
     <!-- Sweet Alert -->
     <script type="text/javascript" src="{{ url() }}/js/sweetalert.min.js"></script>
 
     <!-- My scripts -->
-    <script src="{{ url() }}/js/main.js"></script>
+    <script src="{{ url() }}/js/main.min.js"></script>
 
     </body>
 </html>
