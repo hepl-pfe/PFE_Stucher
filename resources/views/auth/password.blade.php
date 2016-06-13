64be41ed3beba76c2c5e829842e772d1981f31d2
Loïc Parent <loic1994@hotmail.com>
2016-06-13 17:22:47 +0200

    Add a callback message
    => success message
    => hide forms content if success

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -11,26 +11,33 @@
 
 <div class="box--group">
     <div class="box box--shadow box__connect--page">
-        <form class="box__group--content" method="POST" action="/password/email">
-            {!! csrf_field() !!}
-
-            <div class="form-group form-information form-information--password">
-                <p><span class="icon-info icon mainColorfont"></span> En complétant ce formulaire, vous allez recevoir un email afin de pouvoir changer votre mot de passe.</p>
+        @if ( session('status') )
+            <div class="box__group--content box__passwordResetSuccess">
+                <div class="icon icon-envelope-letter"></div>
+                <p class="box__passwordResetSuccess_message">{{ session('status') }}</p>
             </div>
+        @else
+            <form class="box__group--content" method="POST" action="/password/email">
+                {!! csrf_field() !!}
 
-            <div class="form-group">
-                <label class="color_label" for="email">Email</label>
-                <input class="form-control" type="email" name="email" id="email" value="{{ old('email') }}" autofocus>
-            </div>
+                <div class="form-group form-information form-information--password">
+                    <p><span class="icon-info icon mainColorfont"></span> En complétant ce formulaire, vous allez recevoir un email afin de pouvoir changer votre mot de passe.</p>
+                </div>
 
-            <div class="form-group--button">
-                <button class="btn btn-send" type="submit">Envoyez un lien par email</button>
-                <div class="clear"></div>
-            </div>
+                <div class="form-group">
+                    <label class="color_label" for="email">Email</label>
+                    <input class="form-control" type="email" name="email" id="email" value="{{ old('email') }}" autofocus>
+                </div>
 
-            @include( 'errors.profilError' )
+                <div class="form-group--button">
+                    <button class="btn btn-send" type="submit">Envoyez un lien par email</button>
+                    <div class="clear"></div>
+                </div>
 
-        </form>
+                @include( 'errors.profilError' )
+
+            </form>
+        @endif
     </div>
 </div>
 
