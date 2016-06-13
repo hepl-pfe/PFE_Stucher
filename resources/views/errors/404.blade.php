780285953022dd9910be2c4de7e6500c432a4cf7
Loïc Parent <loic1994@hotmail.com>
2016-06-13 17:23:27 +0200

    Add some elements in the 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,11 +2,14 @@
 @section('title', 'Page non trouvée • Stucher')
 @section('content')
 
-    <body class="{{ isset(Auth::user()->color) ? Auth::user()->color : '' }}">
-        <div class="blockTitle">
+    <body class="{{ isset(Auth::user()->color) ? Auth::user()->color : 'default' }}">
+        <div class="blockTitle blockTitle--404">
             <h1 class="mainTitle">Erreur 404</h1>
             <h1 class="subTitle">Hmmm… on dirait que la page n’existe pas.</h1>
 
             <a title="Revenir à la page précédente" class="backButton blockTitle__backButton unlink mainColorfont" href="{{ URL::previous() }}"><span class="hidden">Revenir à la page précédente</span><span class="icon-arrow-left"></span></a>
+        </div>
+        <div class="visual404">
+            <p>404</p>
         </div>
 @stop
